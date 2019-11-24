7f0f1931b52e9cb9f9476f5a355305ce98113a33
Naveen <naveenrathore.214@gmail.com>
2019-11-24 14:38:06 +0530

    initial  commit

---
+++ program/src/program/String.java
@@ -0,0 +1,17 @@
+package program;
+
+import java.util.Scanner;
+
+public class String {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String st = s.nextLine();
+		
+		
+		
+		
+
+	}
+
+}
---
+++ program/src/program/petterns.java
@@ -0,0 +1,21 @@
+package program;
+
+public class petterns{
+static	int k=0;
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) { 
+			//int k=0
+					;
+			k++;
+			for (int j = 0; j <i; j++) {
+				//k++;
+				System.out.print(k);
+				
+		
+			}//k++;
+			System.out.println();
+			
+		}
+	}
+
+}
